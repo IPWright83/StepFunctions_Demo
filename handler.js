31745ecc65456caa5a4441cd9911c3b1b547f0c9
Ian Wright <ian.p.wright@york.ac.uk>
2019-11-29 14:36:11 +0000

    retry logic

--- handler.js
+++ handler.js
@@ -1,2 +1,9 @@
 module.exports.add = async ({ x, y }) => x + y;
 module.exports.double = async n => n * 2;
+
+let counter = 0;
+
+module.exports.counter = async n => {
+    if (++counter > 2) return n;
+    throw new Error(`Counter value is only ${counter}`);
+}
